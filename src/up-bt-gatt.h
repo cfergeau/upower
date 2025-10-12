819232ddcad3f1d9f853735cfdd14401725bad20
Christophe Fergeau <christophe@fergeau.eu>
2025-10-12 14:29:44 +0200

    start adding gatt helpers
    need to understand how to fetch the uuid from a org.bluez.Gatt1
    interface

---
+++ src/up-bt-gatt.h
@@ -0,0 +1,30 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2025 Christophe Fergeau cfergeau@redhat.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef __UP_BT_GATT_H__
+#define __UP_BT_GATT_H__
+
+G_BEGIN_DECLS
+
+const char *gatt_attribute_description(const char *uuid);
+
+G_END_DECLS
+
+#endif /* __UP_BT_GATT_H__ */
